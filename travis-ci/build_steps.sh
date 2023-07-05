81acab0589e42feb2d1718552a52e831c2b7d320
Matthew Barber <quitesimplymatt@gmail.com>
2023-07-05 11:43:06 +0100

    Leverage `OPENBLAS_COMMIT=HEAD` to build nightlies

--- travis-ci/build_steps.sh
+++ travis-ci/build_steps.sh
@@ -59,7 +59,6 @@ function build_lib {
     # Input arg
     #     plat - one of i686, x86_64
     #     interface64 - 1 if build with INTERFACE64 and SYMBOLSUFFIX
-    #     nightly - 1 if building for nightlies
     #
     # Depends on globals
     #     BUILD_PREFIX - install suffix e.g. "/usr/local"
@@ -68,7 +67,6 @@ function build_lib {
     set -x
     local plat=${1:-$PLAT}
     local interface64=${2:-$INTERFACE64}
-    local nightly=${3:0}
     local manylinux=${MB_ML_VER:-1}
     # Make directory to store built archive
     if [ -n "$IS_OSX" ]; then
@@ -110,14 +108,12 @@ function do_build_lib {
     #                         Suffix added with hyphen prefix
     #     interface64 (optional) - whether to build ILP64 openblas
     #                              with 64_ symbol suffix
-    #     nightly (optional) - whether to build for nightlies
     #
     # Depends on globals
     #     BUILD_PREFIX - install suffix e.g. "/usr/local"
     local plat=$1
     local suffix=$2
     local interface64=$3
-    local nightly=$4
     echo "Building with settings: '$plat' '$suffix' '$interface64'"
     case $(get_os)-$plat in
         Linux-x86_64)
@@ -171,7 +167,7 @@ function do_build_lib {
     # && CFLAGS="$CFLAGS -fvisibility=protected" make BUFFERSIZE=20 DYNAMIC_ARCH=1 USE_OPENMP=0 NUM_THREADS=64 BINARY=$bitness $interface64_flags $target_flags > /dev/null \
     # && make PREFIX=$BUILD_PREFIX $interface64_flags install )
     stop_spinner
-    if [ "$nightly" = "1" ]; then
+    if [ "$OPENBLAS_COMMIT" = "HEAD" ]; then
         local version="HEAD"
     else
         local version=$(cd OpenBLAS && git describe --tags --abbrev=8)
